56024c12c44bca53cfc65b95aa58559e305ac5aa
largusopt <mtv.52@yandex.ru>
2025-06-25 10:40:01 +0300

    init

--- src/main/java/com/example/demo/repository/PlayerRepositoryCustom.java
+++ src/main/java/com/example/demo/repository/PlayerRepositoryCustom.java
@@ -3,17 +3,10 @@ package com.example.demo.repository;
 import com.example.demo.DTO.FilterDTO;
 import com.example.demo.filter.PlayerOrder;
 import com.example.demo.model.Player;
-import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface PlayerRepository extends JpaRepository<Player, Long>, PlayerRepositoryCustom{
-    //Player savePlayer(Player player);
-   // List<Player> getPlayers();
-    //void deletePlayer(Player player);
-    //Player getPlayerById(long id);
-
+interface PlayerRepositoryCustom {
     List<Player> getSortedPlayers(FilterDTO filterDTO, PlayerOrder order);
     Integer getPlayersCount(FilterDTO filterDTO);
-
 }
